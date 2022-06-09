a79a28703471f16f9963a0a8f1e8cac06e950053
ShijoyBharath <shijoybharath8@gmail.com>
2022-06-09 14:52:20 +0530

    BusRoute Beautified

--- react-bus-tracker/src/Components/BusRoute.js
+++ react-bus-tracker/src/Components/BusRoute.js
@@ -1,13 +1,20 @@
-import Progess from "./Progess"
+import Progess from "./Progess";
 
 const BusRoute = () => {
   return (
     <div>
-        <div className="pl-14 p-4 m-4 shadow-lg rounded-lg bg-white">
-          <Progess percentage={"76%"}/>
+      <div className="pl-14 p-4 m-4 shadow-lg rounded-lg bg-white">
+        <div className="flex">
+          <Progess percentage={"76%"} />
+          <div className="grid place-content-between p-4 font-semibold">
+            <ul>8th Mile</ul>
+            <ul>8th Mile</ul>
+            <ul>8th Mile</ul>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BusRoute
+export default BusRoute;
--- react-bus-tracker/src/Components/Progess.js
+++ react-bus-tracker/src/Components/Progess.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Progess = ({percentage}) => {
   return (
     <div>
-        <div className=" h-96 bg-gray-200 w-3 rounded-lg">
-            <div className="bg-blue-600 w-3 rounded-lg" style={{"height": percentage }}></div>
+        <div className=" h-screen mt-6 mb-5 bg-gray-200 w-3 rounded-lg">
+            <div className=" bg-blue-600 w-3 rounded-lg" style={{"height": percentage }}></div>
           </div>
     </div>
   )
