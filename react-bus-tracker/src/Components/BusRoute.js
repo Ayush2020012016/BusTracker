4cf404a4d0d88ef5a29e0678c86d2d687a636be5
ShijoyBharath <shijoybharath8@gmail.com>
2022-06-09 14:59:57 +0530

    Added timing to BusRoute.js

--- react-bus-tracker/src/Components/BusRoute.js
+++ react-bus-tracker/src/Components/BusRoute.js
@@ -4,12 +4,18 @@ const BusRoute = () => {
   return (
     <div>
       <div className="pl-14 p-4 m-4 shadow-lg rounded-lg bg-white">
-        <div className="flex">
+        <h1 className="font-bold text-lg">St. John</h1>
+        <div className="flex justify-around">
           <Progess percentage={"76%"} />
           <div className="grid place-content-between p-4 font-semibold">
             <ul>8th Mile</ul>
             <ul>8th Mile</ul>
             <ul>8th Mile</ul>
+          </div>
+          <div className="grid place-content-between p-4 font-semibold text-green-500">
+            <ul>7pm</ul>
+            <ul>7.30pm</ul>
+            <ul>8pm</ul>
           </div>
         </div>
       </div>
