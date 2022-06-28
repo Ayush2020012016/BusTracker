5bcbfe0a9099fdf9dfccdc538719ec5f149ac382
ShijoyBharath <shijoybharath8@gmail.com>
2022-06-28 15:15:28 +0530

    Installed mongodb npm package

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,31 @@
 const app = require('express')();
-const PORT = 3000;
+const PORT = 4000;
 
 app.listen(
     PORT,
     () => console.log(`Server is running on https://localhost:${PORT}`)
 )
+
+app.get('/search', (req,res) => {
+    const from = req.query.from;
+    const to = req.query.to;
+    console.log(`From  : ${from} Destination : ${to}`)
+    res.status(200).send({
+        From : {from},
+        To : {to}
+    })
+
+})
+
+app.get('/busview', (req,res) => {
+    res.status(200).send({
+        Test : "Test"
+    })
+})
+
+
+app.get('/busroute', (req,res) => {
+    res.status(200).send({
+        Test : "Test"
+    })
+})
