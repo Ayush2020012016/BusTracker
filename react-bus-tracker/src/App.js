8ff6b6fdfd769bd827647b598e5389996b06b65c
ShijoyBharath <shijoybharath8@gmail.com>
2022-07-11 00:07:49 +0530

    Testing GeoLocation API in React

--- react-bus-tracker/src/App.js
+++ react-bus-tracker/src/App.js
@@ -1,10 +1,30 @@
 import NavPane from "./Components/NavPane";
 
 function App() {
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      console.log("Geolocation is not supported by your browser");
+    } else {
+      console.log("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log(position.coords.latitude);
+          console.log(position.coords.longitude);
+        },
+        () => {
+          console.log("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  
   return (
-      <div>
-        <NavPane />
-      </div>
+    <div>
+      <NavPane />
+      {getLocation()}
+    </div>
   );
 }
 
