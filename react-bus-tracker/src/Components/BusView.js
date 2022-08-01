fa2065ac5d219584aef3c72a9a99749703d9d603
ShijoyBharath <shijoybharath8@gmail.com>
2022-08-01 23:11:53 +0530

    Changed BusView.js with useEffect

--- react-bus-tracker/src/Components/BusView.js
+++ react-bus-tracker/src/Components/BusView.js
@@ -2,61 +2,47 @@ import React, { useEffect } from "react";
 import BusViewItem from "./BusViewItem";
 import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 // import { useCallback } from "react";
 
 const BusView = () => {
-  const location = useLocation();
-  console.log(location.pathname);
+  // const location = useLocation();
+  // console.log(location.pathname);
 
   const [searchParams, setSearchParams] = useSearchParams();
   const from = searchParams.get("from");
   const to = searchParams.get("to");
 
-  // var idx,namex,timex,destx
 
+
+  const [busdata, setBusdata] = useState("");
   const [idx, setidx] = useState("");
   const [namex, setnamex] = useState("");
   const [timex, settimex] = useState("");
   const [destx, setdestx] = useState("");
 
+  useEffect(() => {
+    fetch(
+          `http://localhost:4000/search?from=${from}&to=${to}`,
+          {
+            headers: { "Access-Control-Allow-Origin": "*" },
+          }
+        ).then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setBusdata(data);
+      });
+  }, []);
 
-  
-//   const [allValues, setAllValues] = useState({
-//     idx: '',
-//     namex: '',
-//     timex: '',
-//     destx: '',
-//  });
+  useEffect(() => {
+    setidx(busdata.id);
+    setnamex(busdata.name);
+    settimex(busdata.time);
+    setdestx(busdata.route);
+  }, [busdata]);
 
-
-  fetchBusDetails(from, to).then((data) => {
-    console.log("BUs view", data);
-    // idx=data.id
-    // namex=data.name
-    // timex=data.time
-    // destx=data.route
-
-
-    setidx(data.id);
-    setnamex(data.name);
-    settimex(data.time);
-    setdestx(data.route);
-
-    // setAllValues({idx:data.id, namex:data.name, timex:data.time, destx:data.route})
-
-  });
-
-  async function fetchBusDetails(from, to) {
-    const responce = await fetch(
-      `http://localhost:4000/search?from=${from}&to=${to}`,
-      {
-        headers: { "Access-Control-Allow-Origin": "*" },
-      }
-    );
-    const data = await responce.json();
-    return data;
-  }
 
   return (
     <div className="m-4 p-3 gap-3 bg-white rounded-lg">
